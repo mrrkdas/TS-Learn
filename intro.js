127ccc724d31337d3677685314cdeb7435ede113
Rishab Das <das.rishabk@gmail.com>
2023-09-08 21:36:05 -0400

    commit

---
+++ intro.js
@@ -0,0 +1,4 @@
+console.log('rishab');
+var user = { name: 'Rishab', age: 19 };
+console.log(user.name);
+console.log('hello');
--- intro.ts
+++ intro.ts
@@ -1,1 +1,6 @@
-console.log('rishab')
+console.log('rishab');
+
+let user = {name:'Rishab', age:19};
+
+console.log(user.name);
+console.log('hello');
