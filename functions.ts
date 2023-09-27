007404cc64f695919bdc8dd0af4bde26446b298c
Rishab Das <das.rishabk@gmail.com>
2023-09-27 17:01:22 -0400

    played out with it

--- functions.ts
+++ functions.ts
@@ -2,3 +2,8 @@ let function1 = (name:string = "hello", age:number = 1) => {console.log(name)};
 
 
 let funtion1 = (height:number = 1, age:number=2) => (console.log(height));
+
+
+function inputX(weight:number, age:number):String{
+    return "hello"; //You can see the way the things change once you do this. 
+}
